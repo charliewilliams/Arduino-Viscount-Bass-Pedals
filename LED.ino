2250a12fc685b0df5e05cc0ca6e858d8aa5f83e3
Charlie Williams <c@charliewilliams.org>
2015-03-11 23:10:08 +0000

    Better colo(u)rs + 'breathing' when idle

--- LED.ino
+++ LED.ino
@@ -1,23 +1,24 @@
 
-#define PIN_R 9
-#define PIN_G 10
-#define PIN_B 11
+#define PIN_R 23
+#define PIN_G 22
+#define PIN_B 21
 
-#define noteColor0 {255, 0, 0} // C.
-#define noteColor1 {230, 126, 34}
-#define noteColor2 {230, 126, 34} // D.
-#define noteColor3 {142, 68, 173} //.
-#define noteColor4 {241, 196, 15} // E.
-#define noteColor5 {251, 191, 149} // F
-#define noteColor6 {127, 127, 127}
-#define noteColor7 {141, 203, 149} // G
-#define noteColor8 {156, 120, 191}
-#define noteColor9 {189, 184, 194} // A
-#define noteColor10 {144, 91, 24}
-#define noteColor11 {192, 139, 72} // B
+#define noteColor0 {90, 0, 0} // C
+#define noteColor1 {9, 0, 20}
+#define noteColor2 {255, 15, 0} // D
+#define noteColor3 {10, 0, 10} //.
+#define noteColor4 {211, 84, 0} // E
+#define noteColor5 {0, 10, 1} // F
+#define noteColor6 {40, 40, 40}
+#define noteColor7 {4, 50, 40} // G
+#define noteColor8 {0, 7, 100}
+#define noteColor9 {0, 0, 34} // A
+#define noteColor10 {20, 0, 0}
+#define noteColor11 {200, 0, 18} // B
 
+//const static int whiteKeys[7][3] = {noteColor0, noteColor2, noteColor4, noteColor5, noteColor7, noteColor9, noteColor11};
 const static int noteColors[12][3] = {noteColor0, noteColor1, noteColor2, noteColor3, noteColor4, noteColor5, noteColor6, noteColor7, noteColor8, noteColor9, noteColor10, noteColor11};
-const static float brightness = 0.25;
+const static float brightness = 1.0;
 bool noteIsOn = false;
 int breathingCounter = 0;
 
@@ -31,7 +32,7 @@ void writeLED(int noteNum, bool on) {
 
   if (!on) {
     noteIsOn = false;
-    breathingCounter = 0;
+//    breathingCounter = 0;
     analogWrite(PIN_R, 0);
     analogWrite(PIN_G, 0);
     analogWrite(PIN_B, 0);
@@ -68,18 +69,9 @@ void updateLED() {
 
   breathingCounter++;
 
-  if (breathingCounter > 255) {
-    breathingCounter = 0;
-  }
-
-  int level = sin(breathingCounter / 255.0);
-
-  analogWrite(PIN_R, level);
-  analogWrite(PIN_G, level);
-  analogWrite(PIN_B, level);
-
-#if DEBUG
-  digitalWrite(13, level);
-#endif
+  float val = (exp(sin(breathingCounter / 200.0 * PI)) - 0.36787944) * 30.0;
+  analogWrite(PIN_R, val);
+  analogWrite(PIN_G, val);
+  analogWrite(PIN_B, val);
 }
 

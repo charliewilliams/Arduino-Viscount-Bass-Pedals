2676036c3c3f3a40e05959f3ac0e44fa93eba898
Charlie Williams <c@charliewilliams.org>
2015-03-16 23:18:42 +0000

    Trying to fix issue with ADSR not going back down to zero

--- Synthesis.ino
+++ Synthesis.ino
@@ -3,22 +3,22 @@
 // switch the synths on and off
 
 #include <Oscil.h>
-//#include <tables/sin2048_int8.h>
+
+#define HIRES_SINWAVE 1 // no big deal which one to use
+#if HIRES_SINWAVE
 #include <tables/sin8192_int8.h>
-
-// use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
-//Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
-
-static const float MASTER_GAIN = 0.02;
-
-//static const int SIN_CELLS = SIN2048_NUM_CELLS;
-//static const int8_t* SIN_DATA = SIN2048_DATA;
 static const int SIN_CELLS = 8192;
 static const int8_t* SIN_DATA = SIN8192_DATA;
+#else
+#include <tables/sin2048_int8.h>
+static const int SIN_CELLS = SIN2048_NUM_CELLS;
+static const int8_t* SIN_DATA = SIN2048_DATA;
+#endif
 
+static const int MASTER_BITSHIFT = 5; // bigger numbers are quieter
 Oscil <SIN_CELLS, AUDIO_RATE> oscils[NUM_KEYS];
 float freqs[NUM_KEYS] = {261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440.000, 466.164, 493.883, 523.251};
-float octave = 2.0;
+float octave = 3.0; // bigger numbers are lower
 
 void setupAudio() {
 
@@ -34,8 +34,12 @@ int updateAudio() {
   int sample = 0.0;
 
   for (int i = 0; i < NUM_KEYS; i++) {
-    sample += oscils[i].next() * envelope[i].next();
+
+    unsigned int env = envelope[i].next();
+
+//    if (env > 4)
+      sample += oscils[i].next() * env;
   }
 
-  return sample * MASTER_GAIN;
+  return sample >> MASTER_BITSHIFT;
 }

5caa7df16a74bcba192ac067e1b32198b9b3b1fc
Charlie Williams <c@charliewilliams.org>
2017-08-01 21:39:46 +0100

    Remove Mozzi. Bye, Mozzi!

--- Mozzi/extras/doc/html/search/all_63.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['char2mozzi_2epy',['char2mozzi.py',['../char2mozzi_8py.html',1,'']]],
-  ['controldelay',['ControlDelay',['../class_control_delay.html',1,'']]]
-];
--- Mozzi/extras/doc/html/search/all_64.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['dcfilter',['DCfilter',['../class_d_cfilter.html',1,'DCfilter'],['../class_d_cfilter.html#ab55e871fc9d11dfb9231e44627181c2c',1,'DCfilter::DCfilter()']]]
-];
